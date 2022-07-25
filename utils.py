96006c6cd7298f5b0a1b77d275e34ccf5ef20f31
Winston <wangsitong1995@outlook.com>
2022-07-26 00:12:46 +0800

    utils.py添加格式化文件大小的函数: format_size

--- utils.py
+++ utils.py
@@ -4,3 +4,14 @@ import time
 def format_time(time_token: float) -> str:
     t = time.localtime(time_token)
     return time.strftime('%Y-%m-%d', t)
+
+
+def format_size(size: int) -> str:
+    units = ('B', 'KB', 'MB', 'GB', 'TB')
+    i = 0
+    while size > 999:
+        size /= 1024
+        i += 1
+        if i == (len(units) - 1):
+            break
+    return f'{round(size, 2)}{units[i]}'
